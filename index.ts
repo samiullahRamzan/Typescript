c675019a080df01397f9fb18c407f3cab8beac71
HP <samidev336@gmail.com>
2025-07-08 22:35:24 +0500

    update

--- index.ts
+++ index.ts
@@ -1,5 +1,12 @@
 console.log('sami');
 
-let a:number=20.5
+// infer..Implicit assignment forces TypeScript to infer the value.
+let a = 20.5;
+// The type of 'a' is inferred as 'number' because it is assigned a numeric value.
+
+// If you want to explicitly declare the type, you can do so like this:
+// let a: number = 20.5;
+
+let message:string="Hello, Samiullah";
 
 console.log(a);
